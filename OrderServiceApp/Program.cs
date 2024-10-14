b2664cc4257489c80767488b13681ffb269023a6
reiss <ilyas.abdrakhmanov07@gmail.com>
2024-10-14 16:05:08 +0500

    VersionFinal

--- OrderServiceApp/Program.cs
+++ OrderServiceApp/Program.cs
@@ -7,23 +7,12 @@ using OrderServiceApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//-- Добавляем конфигурацию для чтения переменных окружения
-builder.Configuration.AddEnvironmentVariables();
-
-//--Получаем строку подключения из переменных окружения
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    .Replace("{Host}", Environment.GetEnvironmentVariable("DB_HOST"))
-    .Replace("{Port}", Environment.GetEnvironmentVariable("DB_PORT"))
-    .Replace("{Database}", Environment.GetEnvironmentVariable("DB_DATABASE"))
-    .Replace("{Username}", Environment.GetEnvironmentVariable("DB_USERNAME"))
-    .Replace("{Password}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
-
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
 builder.Services.AddDbContext<OrderContext>(options =>
-    options.UseNpgsql(connectionString)
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
         .LogTo(Console.WriteLine, LogLevel.Information)); // Добавляем логирование для Entity Framework Core
 
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
